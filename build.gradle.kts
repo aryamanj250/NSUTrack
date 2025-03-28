81150d3710f5484e14a4c377f4940addb00b0e20
aryamanjaiswal <aryamanj250@gmail.com>
2025-03-28 15:54:27 +0530

    fixed the refresh part the calendar part remains

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "8.9.0" apply false
-    id("com.android.library") version "8.9.0" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "1.8.22" apply false  // Downgrade to 1.8.22
 }
